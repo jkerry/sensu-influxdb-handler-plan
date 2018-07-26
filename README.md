eb8b9483457c8725dbf485aff937f26447a7c092
John Kerry <jk185160@ncr.com>
2018-07-26 12:14:10 -0400

    first commit
    Signed-off-by: John Kerry <jk185160@ncr.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# sensu-influxdb-handler
+
+This plan installs the Sensu 2.0 influxdb handler.
+
+## Maintainers
+
+John Kerry <john@kerryhouse.net>
+
+## Type of Package
+
+sensu-influxdb-handler is a binary package.
+
+## Usage
+
+Simply `hab pkg install ncr_devops_platform/sensu-influxdb-handler` and use the binary in your sensu handeler config. Optionally you may binlink with `hab pkg binlink ncr_devops_platform/sensu-influxdb-handler`
---
+++ plan.sh
@@ -0,0 +1,26 @@
+pkg_name=sensu-influxdb-handler
+#pkg_filename=sensuctl-influxdb-handler
+pkg_origin=core
+pkg_version="1.5"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("MIT")
+pkg_source="https://github.com/nikkiki/sensu-influxdb-handler/releases/download/v$pkg_version/${pkg_name}_${pkg_version}_linux_amd64.tar.gz"
+pkg_shasum="8fafc59544d60c0ad1efc2fc10e4213a42d81df3ccca6cf340f45fd37197f500"
+pkg_deps=()
+pkg_bin_dirs=(bin)
+pkg_description="Sensu 2.0 Management Command Line Tool"
+pkg_upstream_url="https://sensu.io"
+
+# do_unpack(){
+#   return 0
+# }
+
+do_build(){
+  return 0
+}
+
+do_install() {
+  build_line "Installing $pkg_name binary"
+  chmod +x "$HAB_CACHE_SRC_PATH/$pkg_name"
+  install -D "$HAB_CACHE_SRC_PATH/$pkg_name" "$pkg_prefix/bin/$pkg_name"
+}
